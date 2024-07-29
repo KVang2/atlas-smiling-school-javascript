282980a81e12b73031385a4617b1b77e921e83f1
kvang2 <kevinvaj88@hotmail.com>
2024-07-29 16:21:02 -0500

    pushing save point

--- scripts.js
+++ scripts.js
@@ -1,54 +1,44 @@
-// AJAX Request
-
 $(document).ready(function() {
-    $('#loader').show();
-    fetchCarouselData();
+    fetchQuoteData();
 });
 
-function fetchCarouselData() {
+function fetchQuoteData() {
+    $('.loader').show();
+
     $.ajax({
         url: 'https://smileschool-api.hbtn.info/quotes',
         method: 'GET',
         success: function(data) {
-            $('#loader').hide();
-            data.forEach(function(quote, index) {
-                var activeClass = index === 0 ? ' active' : '';
-                var slide = `
-                    <div class="carousel-item${activeClass}">
-                        <div class="row mx-auto align-items-center">
-                            <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
-                                <img
-                                    src="${quote.pic_url}"
-                                    class="d-block align-self-center"
-                                    alt="${quote.name}"
-                                />
-                            </div>
-                            <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
-                                <div class="quote-text">
-                                    <p class="text-white">« ${quote.quote}</p>
-                                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
-                                    <span class="text-white">${quote.title}</span>
+            $('.loader').hide();
+
+            data.forEach(function(quote) {
+                var slide = `<div class="carousel-item d-flex flex-row align-items-center justify-content-center pr-4 pl-4 m-0">
+                                <img src="${quote.pic_url}" alt="Image" class="img-fluid">
+                                <div class="container">
+                                    <p class="quote-text">${quote.quote}</p>
+                                    <p class="quote-text"><strong>${quote.name}</strong></p>
+                                    <p class="quote-text"><em>${quote.title}</em></p>
                                 </div>
-                            </div>
-                        </div>
-                    </div>`;
-                $('.carousel-inner').append(slide);
+                            </div>`;
+                $('.carousel-single').append(slide);
             });
-            initializeCarousel();
+
+            initializeSlickCarousel();
         },
         error: function() {
-            $('#loader').hide();
-            alert('Failed to load');
+            $('.loader').hide();
+            alert('Failed to load quotes');
         }
     });
 }
 
 function initializeSlickCarousel() {
-    $('.carousel-inner').slick({
+    $('.carousel-single').slick({
         arrows: true,
-        prevArrow: '<button type="button" class="slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-prev"></button>',
-        speed:200,
+        prevArrow: '<img src="/images/arrow_white_left.png" class="slick-prev"/>',
+        nextArrow: '<img src="/images/arrow_white_right.png" class="slick-next"/>',
+        speed: 200,
         slidesToShow: 1,
+        adaptiveHeight: true
     });
 }
