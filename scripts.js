94927a6232db304ec7271a0f135b0f2ba68226eb
kvang2 <kevinvaj88@hotmail.com>
2024-07-29 21:06:04 -0500

    save

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function fetchQuoteData() {
             data.forEach(function(quote) {
                 var slide = `<div class="carousel-item d-flex flex-row align-items-center justify-content-center pr-4 pl-4 m-0">
                                 <img src="${quote.pic_url}" alt="${quote.name}" class="img-fluid">
-                               <div class="container">
+                                <div class="container">
                                     <p class="quote-text text-white">${quote.text}</p>
                                     <h4 class="quote-text text-white font-weight-bold">${quote.name}</h4>
                                     <span class="quote-text text-white">${quote.title}</span>
