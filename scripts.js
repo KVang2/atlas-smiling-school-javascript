c430d23a821710bb99ed3ce0dad3d8bc4e44d75f
kvang2 <kevinvaj88@hotmail.com>
2024-07-31 14:03:56 -0500

    pushing script for video

--- scripts.js
+++ scripts.js
@@ -55,77 +55,31 @@ function fetchVideoData() {
         url: 'https://smileschool-api.hbtn.info/popular-tutorials',
         method: 'GET',
         success: function(data) {
+            console.log(data);
             $('.loader').hide();
 
             data.forEach(function(video) {
-                var stars = calculateRatingStars(video);
-                var slide = `<div class="row align-items-center mx-auto">
-                  <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+                var stars = RatingStars(video);
+                var slide = `
                     <div class="card">
-                      <img
-                        src="images/thumbnail_4.jpg"
-                        class="card-img-top"
-                        alt="Video thumbnail"
-                      />
-                      <div class="card-img-overlay text-center">
-                        <img
-                          src="images/play.png"
-                          alt="Play"
-                          width="64px"
-                          class="align-self-center play-overlay"
-                        />
-                      </div>
-                      <div class="card-body">
-                        <h5 class="card-title font-weight-bold">
-                          Diagonal Smile
-                        </h5>
-                        <p class="card-text text-muted">
-                          Lorem ipsum dolor sit amet, consect adipiscing elit,
-                          sed do eiusmod.
-                        </p>
-                        <div class="creator d-flex align-items-center">
-                          <img
-                            src="images/profile_1.jpg"
-                            alt="Creator of
-                            Video"
-                            width="30px"
-                            class="rounded-circle"
-                          />
-                          <h6 class="pl-3 m-0 main-color">Phillip Massey</h6>
+                        <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
+                        <div class="card-img-overlay text-center">
+                            <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
                         </div>
-                        <div class="info pt-3 d-flex justify-content-between">
-                          <div class="rating">
-                            <img
-                              src="images/star_on.png"
-                              alt="star on"
-                              width="15px"
-                            />
-                            <img
-                              src="images/star_on.png"
-                              alt="star on"
-                              width="15px"
-                            />
-                            <img
-                              src="images/star_on.png"
-                              alt="star on"
-                              width="15px"
-                            />
-                            <img
-                              src="images/star_on.png"
-                              alt="star on"
-                              width="15px"
-                            />
-                            <img
-                              src="images/star_off.png"
-                              alt="star on"
-                              width="15px"
-                            />
-                          </div>
-                          <span class="main-color">8 min</span>
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">${video.title}</h5>
+                            <p class="card-text text-muted">Lorem ipsum dolor sit amet, consect adipiscing elit,
+                          sed do eiusmod.</p>
+                            <div class="creator d-flex align-items-center">
+                                <img src="${video.author_pic_url}" alt="Creator" width="30px" class="rounded-circle">
+                                <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                            </div>
+                            <div class="info pt-3 d-flex justify-content-between">
+                                <div class="rating">${stars}</div>
+                                <span class="main-color">8 min</span>
+                            </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>`;
+                    </div>`;
                 $('.carousel-video').append(slide);
             });
 
@@ -133,19 +87,31 @@ function fetchVideoData() {
         },
         error: function() {
             $('.loader').hide();
-            alert('Failed to load quotes');
+            alert('Failed to load video');
         }
     });
 }
 
+function RatingStars(rating) {
+    let stars = '';
+    for (let i =0; i < 5; i++) {
+        if (i < rating) {
+            stars += '<img src="images/star_on.png" alt="star-on" width="15px"/>';
+        } else {
+            stars += '<img src="images/star_off.png" alt="star-off" width="15px"/>';
+        }
+    }
+    return stars;
+}
+
 function initializeVideoCarousel() {
     $('.carousel-video').slick({
-        arrows: true,
-        prevArrow: '<img src="/images/arrow_white_left.png" class="slick-prev"/>',
-        nextArrow: '<img src="/images/arrow_white_right.png" class="slick-next"/>',
+        infinite: true,
+        prevArrow: '<img src="/images/arrow_black_left.png" class="video-prev"/>',
+        nextArrow: '<img src="/images/arrow_black_right.png" class="video-next"/>',
         speed: 300,
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 768,
